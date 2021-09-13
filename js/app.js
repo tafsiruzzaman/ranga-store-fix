bd55bdd5f53d2194be64d8f6ade289ab6739a0d2
tafsiruzzaman <tafsirjaman100@gmail.com>
2021-09-13 22:39:17 +0600

    almost done

--- js/app.js
+++ js/app.js
@@ -113,7 +113,28 @@ const showProducts = (products) => {
           <span>(${product.rating.count})</span>
           <div class="mt-3">
             <button type="button" class="btn btn-info me-2" onclick="addToCart(${product.id},${product.price})">Add to cart</button>
-            <button type="button" class="btn btn-warning">Details</button>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+              Details
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Product Description</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    ${product.description}
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         </div>
